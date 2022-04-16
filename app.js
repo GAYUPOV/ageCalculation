eb907889cdd714081701aefc889907986a347cce
unknown <PROWEB-Aybek@8-2.intranet.proweb.uz>
2022-04-16 13:49:51 +0500

    ageCalculation

---
+++ app.js
@@ -0,0 +1,15 @@
+let userName = prompt("Assalomu Aleykum Ismingizni Kiriting");
+while (!isNaN(userName) || userName == "") {
+    userName = prompt("Assalomu Aleykum Ismingizni Kiriting");
+}
+let currentYear = 2022;
+let userYear = +prompt(userName + " Tug'ilgan Yilingizni Kiriting");
+while (isNaN(userYear) || userYear == "" || userYear <= 2000 || userYear >= 2010) {
+    userYear = +prompt(userName + " Tug'ilgan Yilingizi Kiriting")
+}
+
+function calcAge(a, b) {
+    let age = a - b;
+    return age;
+}
+alert(userName + " Siz " + calcAge(currentYear, userYear) + " Yoshda Da Ekansiz !!!");
